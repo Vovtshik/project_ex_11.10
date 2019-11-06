413cd1f0991f98d2e7372255cce10b18832b162b
Volodymir Konovalov <vvkon@meta.ua>
2019-11-06 09:03:31 +0200

    Update SourceE1110.cpp

--- SourceE1110.cpp
+++ SourceE1110.cpp
@@ -4,11 +4,22 @@ vector<string>split(const string& s);
 
 int main()
 {
-
+    string str("Two little birds\nSitting on a wall,\nOne called Peter,\nOne called Paul.\nFly away Peter,\nFly away Paul,\nCome back Peter,\nCome back Paul.\n");
+    vector<string>vstr(split(str));
+    for(string s: vstr)
+    { 
+        cout << s << "\n";
+    }
     return 0;
 }
 
 vector<string>split(const string& s)
 {
-    
+    vector<string>vs;
+    stringstream ss(s);
+    for(string tmp; ss >> tmp;)
+    {
+        vs.push_back(tmp);
+    }
+    return vs;
 }
